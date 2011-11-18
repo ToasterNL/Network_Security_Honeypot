2768bc2acf1a8fc1fd6912835187c93a718d09b6
Aram Verstegen <aram.verstegen@gmail.com>
2011-11-18 17:06:24 +0100

    Using rsync like a sane person

--- configs/backup.sh
+++ configs/backup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 TIME=`date +%s`
-BACKUP_DIR="backups/backup_${TIME}"
+BACKUP_DIR="backups"
 LOG_DIR="/home/util139/logs/"
 mkdir ${BACKUP_DIR}
-cp -R ${LOG_DIR} ${BACKUP_DIR}
+#cp -R ${LOG_DIR} ${BACKUP_DIR}
+rsync -ar --max-delete=0 ${LOG_DIR} ${BACKUP_DIR}
 
